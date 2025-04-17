7cf01717b357b4eca2853f2ab2b50ee3dd08130a
jhand655 <jasonhand@autoworkssc.com>
2025-04-17 13:32:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def score_customer(
     has_checking_account, down_payment
 ):
     credit_score_norm = normalize(credit_score, 400, 700)
-    income_norm = normalize(income, 19200, 60000)
+    income_norm = normalize(income, 19200, 200000)
     residence_time_norm = normalize(time_at_residence, 0, 3)
     prev_job_time_norm = normalize(time_at_prev_job, 0, 3)
     down_payment_norm = normalize(down_payment, 1500, 10000)
@@ -49,15 +49,15 @@ def score_customer(
 st.set_page_config(page_title="Customer Scoring App", layout="centered")
 st.title("🏁 Dealership Customer Scoring")
 
-credit_score = st.number_input("Credit Score", 400, 700, value=580)
-income = st.number_input("Annual Income ($)", 19200, 60000, value=40000)
+credit_score = st.number_input("Credit Score", 400, 700, value=560)
+income = st.number_input("Annual Income ($)", 19200, 60000, value=36000)
 time_at_job = st.number_input("Time at Current Job (years)", 0.0, 50.0, value=1.0)
-time_at_residence = st.number_input("Time at Residence (years)", 0.0, 50.0, value=2.0)
-prev_job_time = st.number_input("Time at Previous Job (years)", 0.0, 50.0, value=3.0)
-down_payment = st.number_input("Down Payment Amount ($)", 1500, 10000, value=2500)
+time_at_residence = st.number_input("Time at Residence (years)", 0.0, 50.0, value=1)
+prev_job_time = st.number_input("Time at Previous Job (years)", 0.0, 50.0, value=2)
+down_payment = st.number_input("Down Payment Amount ($)", 1500, 10000, value=1500)
 
 prev_repossession = st.radio("Previous Repossessions?", ("No", "Yes"))
-num_repos = st.slider("How many repossessions?", 1, 5, 1) if prev_repossession == "Yes" else 0
+num_repos = st.number("How many repossessions?", 1, 5, 1) if prev_repossession == "Yes" else 0
 has_checking_account = st.radio("Do you have a checking account?", ("Yes", "No"))
 
 if st.button("Calculate Score"):
