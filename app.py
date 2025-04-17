9d02948fac524d6c185ed84400b2dc0f47abff3c
jhand655 <jasonhand@autoworkssc.com>
2025-04-17 13:45:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def score_customer(
     has_checking_account, down_payment
 ):
     credit_score_norm = normalize(credit_score, 400, 700)
-    income_norm = normalize(income, 19200, 200000)
+    income_norm = normalize(income, 19200, 40000)
     residence_time_norm = normalize(time_at_residence, 0, 3)
     prev_job_time_norm = normalize(time_at_prev_job, 0, 3)
     down_payment_norm = normalize(down_payment, 1500, 10000)
